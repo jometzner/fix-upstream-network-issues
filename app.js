83e3cee8fb84a5238a45c3c21334042215074b6d
Johannes Metzner <J.Metzner@intershop.de>
2022-12-16 15:10:33 +0100

    Code to test upstream connection issues

--- app.js
+++ app.js
@@ -1,46 +1,76 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4200
+const client = require('prom-client');
+const collectDefaultMetrics = client.collectDefaultMetrics;
 const http = require('http')
+const keepSocketAliveFct = http.Agent.prototype.keepSocketAlive;
+http.Agent.prototype.keepSocketAlive = function keepSocketAlive(socket) {
+	console.log('keepSocketAlive', this.totalSocketCount, this.maxTotalSockets, this.maxSockets, this.maxFreeSockets);
+	let ret = keepSocketAliveFct.call(this, socket);
+	return ret;
+};
+const agent = new http.Agent({keepAlive: true});
+agent.on('free', (socket, options) => {
+	console.log('free', agent.totalSocketCount, agent.maxTotalSockets, agent.maxSockets, agent.maxFreeSockets);
+});
+const options = {agent, protocol: 'http:'};
+const baseUrl = 'http://pwa-ish-demo.test.intershop.com/INTERSHOP/rest/WFS/inSPIRED-inTRONICS_Business-Site/rest';
 
-app.get('/', (req, res) => {
-	http.get('http://localhost:8000/', (res) => {
-		const { statusCode } = res;
-		const contentType = res.headers['content-type'];
+app.get('/', (expressReq, expressRes) => {
+	for (let index = 0; index < 1000000; index++) {
+		const element = index + index;
+	}
+	processClientRequest(http.get(`${baseUrl}/configurations`, options, processResponse));
+	for (let index = 0; index < 1000000; index++) {
+		const element = index + index;
+	}
+	processClientRequest(http.get(`${baseUrl};loc=en_US;cur=USD/cms/includes/include.homepage.content.pagelet2-Include`, options, processResponse));
+	for (let index = 0; index < 1000000; index++) {
+		const element = index + index;
+	}
+	processClientRequest(http.get(`${baseUrl};loc=en_US;cur=USD/categories?imageView=NO-IMAGE&view=tree&limit=2&omitHasOnlineProducts=true`, options, processResponse));
+	for (let index = 0; index < 1000000; index++) {
+		const element = index + index;
+	}
+	expressRes.end();
+})
 
-		let error;
-		// Any 2xx status code signals a successful response but
-		// here we're only checking for 200.
-		if (statusCode !== 200) {
-		  error = new Error('Request Failed.\n' +
-							`Status Code: ${statusCode}`);
-		} else if (!/^application\/json/.test(contentType)) {
-		  error = new Error('Invalid content-type.\n' +
-							`Expected application/json but received ${contentType}`);
-		}
-		if (error) {
-		  console.error(error.message);
-		  // Consume response data to free up memory
-		  res.resume();
-		  return;
-		}
-
-		res.setEncoding('utf8');
-		let rawData = '';
-		res.on('data', (chunk) => { rawData += chunk; });
-		res.on('end', () => {
-		  try {
-			const parsedData = JSON.parse(rawData);
-			console.log(parsedData);
-		  } catch (e) {
-			console.error(e.message);
-		  }
-		});
-	  }).on('error', (e) => {
-		console.error(`Got error: ${e.message}`);
-	  });
+app.get('/metrics', (_, res) => {
+	client.register.metrics().then(content => {
+		res.set('Content-Type', client.contentType);
+		res.send(content);
+	})
+	.catch(error => {
+		res.status(500).send(error.toString());
+	});
 })
 
 app.listen(port, () => {
+  collectDefaultMetrics();
   console.log(`Example app listening on port ${port}`)
 })
+
+function processResponse(res) {
+	let rawData = '';
+	res.on('data', (chunk) => { rawData += chunk; });
+	res.on('error', (err) => { console.error(err)});
+	res.on('end', () => {
+	  try {
+		const parsedData = JSON.parse(rawData);
+	  } catch (e) {
+		console.log(e.message);
+	  }
+	});
+}
+
+function processClientRequest(clientRequest) {
+	clientRequest.on('error', (e) => {
+		console.log(`Got error: ${e.message}`);
+	});
+
+	clientRequest.on('socket', (socket) => {
+
+	});
+
+}
